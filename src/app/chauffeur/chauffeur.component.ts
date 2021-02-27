629cecd4dae8815812dd0f32630378428bf5a674
ayyoubb96 <ayousito@gmail.com>
2021-02-27 11:40:44 +0100

    commit de ayoub

--- src/app/chauffeur/chauffeur.component.ts
+++ src/app/chauffeur/chauffeur.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chauffeur.component.css']
 })
 export class ChauffeurComponent implements OnInit {
+  showVarq: boolean = false;
+  acceptq: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleChild() {
+    this.showVarq = false;
+    this.acceptq =true;
+  }
+
+  toggleChild1() {
+    this.acceptq =false;
+    this.showVarq = true;
+  }
 }
